5ab35ad909b06601f03dbf609876830258bdf21f
JEONG-MW <51899261+Jeong-Mw@users.noreply.github.com>
2020-11-02 23:57:08 +0900

    .
    Random array

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         public bool swich;
-        public bool[] arrayLed = new bool[4];
+        public bool[] arrayLed = new bool[3];
         public Form1()
         {
             InitializeComponent();
@@ -33,28 +33,34 @@ namespace WindowsFormsApp1
             }
 
         }
-        static int RandomNumber(int min, int max)
+        public void RandomArray(Random d1, bool[] result)
         {
-            Random random = new Random(); 
-            
-            return random.Next(min, max);
+            byte[] b1 = new byte[3];
+            d1.NextBytes(b1);
+
+            for (int i = 0; i <3; i++)
+            {
+                if (b1[i]%2==0)
+                {
+                    result[i] = true;
+                }
+                else
+                {
+                    result[i] = false;
+                }
+            }
+            Console.WriteLine();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Console.WriteLine(RandomNumber(0, 2));
+            Random r1 = new Random();
 
-            for (int i = 0; i < 4; i++)
+            RandomArray(r1, arrayLed);
+
+            foreach(bool bo in arrayLed)
             {
-                if (RandomNumber(0, 2)==0)
-                {
-                    arrayLed[i] = true;
-                }
-                else
-                {
-                    arrayLed[i] = false;
-                }
-                Console.WriteLine(arrayLed[i]);
+                Console.WriteLine("{0,5}", bo);
             }
         }
     }
