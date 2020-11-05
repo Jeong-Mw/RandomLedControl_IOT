3255422def0dea01c51c40364693afc8e87f779c
JEONG-MW <51899261+Jeong-Mw@users.noreply.github.com>
2020-11-05 11:51:04 +0900

    .
    GUI수정

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,11 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -44,31 +41,11 @@
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(80, 196);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ON/OFF";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 40F);
-            this.label1.Location = new System.Drawing.Point(71, 118);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 54);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "OFF";
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(80, 237);
+            this.button2.Location = new System.Drawing.Point(47, 203);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(150, 70);
             this.button2.TabIndex = 3;
             this.button2.Text = "Swiching";
             this.button2.UseVisualStyleBackColor = true;
@@ -76,23 +53,13 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(657, 88);
+            this.button3.Location = new System.Drawing.Point(657, 114);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
             this.button3.Text = "connect";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("굴림", 28F);
-            this.label2.Location = new System.Drawing.Point(481, 71);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 38);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "DISCON";
             // 
             // richTextBox1
             // 
@@ -194,9 +161,6 @@
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -205,12 +169,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
